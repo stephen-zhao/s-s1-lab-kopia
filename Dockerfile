6f48634d1b970ac61a3e9028af8755c23640778c
Stephen Zhao <mail@zhaostephen.com>
2024-04-20 14:32:51 -0400

    Add start script for passwords read from file

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM      kopia/kopia:latest
+
+COPY      start-kopia-server.sh /bin
+
+RUN       chmod a+x /bin/start-kopia-server.sh
+
---
+++ start-kopia-server.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+# Environment:
+#
+# KOPIA_SERVER_PASSWORD_FILE
+#     Path to a secret file containing value for --server-password arg
+#     (password for --server-username arg). Used to log in to the web UI.
+#
+# KOPIA_SERVER_CONTROL_PASSWORD_FILE
+#     Path to a secret file containing value for --server-control-password
+#     arg (password for --server-control-username arg). Used to authenticate
+#     when using `kopia server` CLI.
+#
+# KOPIA_PASSWORD_FILE
+#     Path to a secret file containing value for KOPIA_PASSWORD env var
+#     (password for USER env var). Used as the repository password.
+
+# === Docker Swarm Secrets compatibility ===
+# Read secrets from files and pass to kopia server start
+
+# Read server password from file:
+if [[ -f "${KOPIA_SERVER_PASSWORD_FILE}" ]]; then
+  KOPIA_SERVER_PASSWORD=$(cat "${KOPIA_SERVER_PASSWORD_FILE}")
+fi
+
+# Read server control password:
+if [[ -f "${KOPIA_SERVER_CONTROL_PASSWORD_FILE}" ]]; then
+  KOPIA_SERVER_CONTROL_PASSWORD=$(cat "${KOPIA_SERVER_CONTROL_PASSWORD_FILE}")
+fi
+
+# Read repository password from file:
+if [[ -f "${KOPIA_PASSWORD_FILE}" ]]; then
+  KOPIA_PASSWORD=$(cat "${KOPIA_PASSWORD_FILE}")
+fi
+
+# === Parse additional args read in from environment ===
+
+ADDITIONAL_ARGS=()
+i=0
+
+if [[ -n "${KOPIA_SERVER_PASSWORD}" ]]; then
+  ADDITIONAL_ARGS[$i]="--server-password=${KOPIA_SERVER_PASSWORD}"
+  ((++i))
+fi
+
+if [[ -n "${KOPIA_SERVER_CONTROL_PASSWORD}" ]]; then
+  ADDITIONAL_ARGS[$i]="--server-control-password=${KOPIA_SERVER_CONTROL_PASSWORD}"
+  ((++i)
+fi
+
+# === Replace process with kopia server ===
+
+exec kopia server start "$@" "${ADDITIONAL_ARGS[@]}"
+
